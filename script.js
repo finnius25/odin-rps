b80f6ecea9273d92f44be4c5327dd7600adf57bd
finnius25 <finneykoshy@gmail.com>
2021-10-26 17:41:19 -0500

    create playRound func & computPlay func

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
-console.log("Hello World!")
+const option = ["rock", "paper", "scissor"];
 
-let a = 1; 
+let computerPlay = (option) => {
+    let i = Math.floor(Math.random() * option.length)
+    return option[i];
+}
 
-console.log(a + a);
+let playRound = (playerSelection, computerSelection) => {
+    if (playerSelection == "rock" && computerSelection == "paper") {
+        console.log("You lose! Paper beats Rock");
+    } else if (playerSelection == "rock" && computerSelection == "scissor") {
+        console.log("You win! Rock beats Scissor");
+    } else {
+        console.log("It's a tie! Play again");
+    }
+}
+
+
+const computerSelection = computerPlay();
+const playerSelection = "rock";
+
+
+
+
+
+
