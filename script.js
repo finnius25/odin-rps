75ad544c442ab96041f8bbb732fe6729d5ca9c87
finnius25 <finneykoshy@gmail.com>
2021-10-26 21:23:22 -0500

    create basic ver of game

--- script.js
+++ script.js
@@ -1,23 +1,47 @@
-const option = ["rock", "paper", "scissor"];
+const rps = ["rock", "paper", "scissor"];
 
-let computerPlay = (option) => {
-    let i = Math.floor(Math.random() * option.length)
-    return option[i];
+let playerPlay = () => {
+    return prompt("Rock Paper Scissor GO!").toLowerCase();
+}
+
+let computerPlay = (options) => {
+    let i = Math.floor(Math.random() * options.length)
+    return options[i];
 }
 
 let playRound = (playerSelection, computerSelection) => {
-    if (playerSelection == "rock" && computerSelection == "paper") {
-        console.log("You lose! Paper beats Rock");
-    } else if (playerSelection == "rock" && computerSelection == "scissor") {
-        console.log("You win! Rock beats Scissor");
+    if (playerSelection === "rock" && computerSelection === "paper") {
+        return ("You lose! Paper beats Rock");
+    } else if (playerSelection === "rock" && computerSelection === "scissor") {
+        return ("You win! Rock beats Scissor");
+    } else if (playerSelection === "rock" && computerSelection === "rock") {
+        return ("It's a tie! Play again");
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        return ("You win! Paper beats Rock");
+    } else if (playerSelection === "paper" && computerSelection === "scissor"){
+        return ("You lose! Scissor beats Paper");
+    } else if (playerSelection === "paper" && computerSelection === "paper") {
+        return ("It's a tie! Play again");
+    } else if (playerSelection === "scissor" && computerSelection === "rock") {
+        return ("You lose! Rock beats Scissors");
+    } else if (playerSelection === "scissor" && computerSelection === "paper") {
+        return ("You win! Scissors beats Paper"); 
+    } else if (playerSelection === "scissor" && computerSelection === "scissor") {
+        return ("It's a tie! Play again")
     } else {
-        console.log("It's a tie! Play again");
+        return ("Whoops! Something went wrong")
     }
 }
 
 
-const computerSelection = computerPlay();
-const playerSelection = "rock";
+const computerSelection = computerPlay(rps);
+const playerSelection = playerPlay();
+
+console.log(playRound(playerSelection, computerSelection));
+
+
+
+
 
 
 
