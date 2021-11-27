0cc1a1db7b6f918a767299444d4a2054db78360b
finnius25 <finneykoshy@gmail.com>
2021-11-27 01:56:35 -0600

    everything was created

--- script.js
+++ script.js
@@ -1,65 +1,112 @@
+const game = () => {
+    let pScore = 0;
+    let cScore = 0;
 
-const computerPlay = () => {
-    const rps = ["rock", "paper", "scissor"];
-    return rps[Math.floor(Math.random() * rps.length)]
-}
+// start game
+    const startGame = () => {
+        const playBtn = document.querySelector('.intro button');
+        const introScreen = document.querySelector('.intro');
+        const match = document.querySelector('.match');
 
+        playBtn.addEventListener('click', () => {
+            introScreen.classList.add("fadeOut");
+            match.classList.add("fadeIn");
+        });
+    };
 
-const playRound = (playerSelection, computerSelection) => {
-    computerSelection = computerPlay();
-    if (
-    (computerScore <= 5 && playerScore <= 5) &&
-    (playerSelection === "rock" && computerSelection === "scissor") ||
-    (playerSelection === "paper" && computerSelection === "rock") ||
-    (playerSelection === "scissor" && computerSelection === "paper") {
-        playerScore ++;
-        roundNum ++;
+    const playMatch = () => {
+        const options = document.querySelectorAll('.player-btns button');
+        const playerHand = document.querySelector('.player-hand');
+        const computerHand = document.querySelector('.computer-hand');
+        const hands = document.querySelectorAll('.hands img');
+
+        hands.forEach(hand => {
+            hand.addEventListener('animationend', function() {
+                this.style.animation = '';
+            });
+        });
+
+        const computerOptions = ['rock', 'paper', 'scissors'];
+
+        options.forEach(option => {
+            option.addEventListener('click', function() {
+                
+                const computerNumber = Math.floor(Math.random() * 3);
+                const computerChoice = computerOptions[computerNumber];
+
+                setTimeout(() => {
+                    compareHands(this.textContent, computerChoice);
+                    playerHand.src = `./assets/${this.textContent}.png`;
+                    computerHand.src = `./assets/${computerChoice}.png`;    
+                }, 2000);
+
+                playerHand.style.animation = "shakePlayer 2s ease";
+                computerHand.style.animation = "shakeComputer 2s ease";
+            });
+        });
+    };
+
+    const updateScore = () => {
+        const playerScore = document.querySelector('.player-score p');
+        const computerScore = document.querySelector('.computer-score p');
+        playerScore.textContent = pScore; 
+        computerScore.textContent = cScore;    
+    }
+
+    const compareHands = (playerChoice, computerChoice) => {
         
+        const winner = document.querySelector('.winner');
+        
+        if (playerChoice === computerChoice) {
+            winner.textContent = "It's a tie!";
+            return;
+        }
+
+        if (playerChoice === 'rock') {
+            if (computerChoice === 'scissors') {
+                winner.textContent = "Player Wins!";
+                pScore++;
+                updateScore();
+                return;
+            }else {
+                winner.textContent = "Computer Wins!";
+                cScore++;
+                updateScore();
+                return; 
+            };
+        }
+
+        if (playerChoice === 'paper') {
+            if (computerChoice === 'scissors') {
+                winner.textContent = "Computer Wins!";
+                cScore++;
+                updateScore();
+                return;
+            }else {
+                winner.textContent = "Player Wins!";
+                pScore++;
+                updateScore();
+                return; 
+            };
+        }
+
+        if (playerChoice === 'scissors') {
+            if (computerChoice === 'paper') {
+                winner.textContent = "Player Wins!";
+                pScore++;
+                updateScore();
+                return;
+            }else {
+                winner.textContent = "Computer Wins!";
+                cScore++;
+                updateScore();
+                return; 
+            };
+        }
     }
 
-    (playerSelection === "rock" && computerSelection === "paper") 
-    (playerSelection === "paper" && computerSelection === "scissor")
-    (playerSelection === "scissor" && computerSelection === "rock") 
+    startGame();
+    playMatch();
+};
 
-    (playerSelection === "rock" && computerSelection === "rock") 
-    (playerSelection === "paper" && computerSelection === "paper") 
-    (playerSelection === "scissor" && computerSelection === "scissor") 
-    
-    } else {
-        return ("Whoops! Something went wrong")
-    }
-}
-
-const rock = document.querySelector('#rock');
-const paper = document.querySelector('#paper');
-const scissor = document.querySelector('#scissor');
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// rock.addEventListener('click', () => {
-//     alert(playRound('rock'));
-// });
-// paper.addEventListener('click', () => {
-//     alert(playRound('paper'));
-// });
-// scissor.addEventListener('click', () => {
-//     alert(playRound('scissor'));
-// });
-
-
-
-
-
-
+game();
